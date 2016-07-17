3b215ae03fbee68f1b0d2da5f516be3b9a85fbb9
Vikas Verma <vikk2411@gmail.com>
2016-07-17 23:11:55 +0530

    Devise User and Annotate installed

---
+++ spec/factories/users.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  email                  :string
+#  username               :string
+#  phone                  :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#
+
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+    email "MyString"
+    username ""
+    phone ""
+  end
+end
